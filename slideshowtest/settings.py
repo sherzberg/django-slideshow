0544624f4a76c5e317ca9de6fd78d5cec1e9cfcb
Spencer Herzberg <spencer.herzberg@gmail.com>
2011-08-19 15:12:23 -0400

    added slideshow get_absolute_url

--- slideshowtest/settings.py
+++ slideshowtest/settings.py
@@ -95,6 +95,9 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    
+    'south',
+    
     'slideshow',
 )
 
